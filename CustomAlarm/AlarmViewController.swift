bdc0961142db48fe4df07c2bdb4f208074557b6f
daxapps <daxapps777@gmail.com>
2017-03-10 13:03:47 -0600

    Create ViewControllers, Connect Outlets and Actions

---
+++ CustomAlarm/AlarmViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AlarmViewController.swift
+//  CustomAlarm
+//
+//  Created by Jason Crawford on 3/10/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UITableViewController {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var caption: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tapToSelectImage: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+    }
+
+    @IBAction func imageViewTapped(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
